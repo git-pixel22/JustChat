6e8905d140982656a41b4f18ee5a40c64b0435af
git-pixel22 <workemail.rajat@gmail.com>
2024-06-09 13:56:19 +0530

    basic setup done

--- public/js/main.js
+++ public/js/main.js
@@ -1,2 +1,42 @@
+const chatForm = document.getElementById('chat-form');
+const chatMessages = document.querySelector('.chat-messages');
+
 const socket = io();
 
+// Message from server
+socket.on('message', message => {
+    console.log(message);
+
+    // Display message on the frontend UI
+    outputMessage(message);
+
+    // Scroll down
+    chatMessages.scrollTop = chatMessages.scrollHeight;
+})
+
+// On form submit send the message to the server.
+chatForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    // Get message text
+    const msg = e.target.elements.msg.value;
+
+    // Emit message to the backend server
+    socket.emit('chatMessage', msg);
+
+    // Clear Input And Focus On The Field
+    e.target.elements.msg.value = '';
+    e.target.elements.msg.focus();
+})  
+
+//Output Message to DOM
+function outputMessage(message) {
+    const div = document.createElement('div');
+    div.classList.add('message');
+    div.innerHTML = `<p class="meta">${message.username} <span>${message.time}</span></p>
+    <p class="text">
+        ${message.text}
+    </p>`;
+
+    document.querySelector('.chat-messages').appendChild(div);
+}
--- server.js
+++ server.js
@@ -1,18 +1,37 @@
 import express from "express";
 import http from "http";
 import { Server } from 'socket.io';
+import { formatMessage } from "./utils/message.js";
 
 const app  = express();
 const server = http.createServer(app)
 const io = new Server(server);
 
+const botName = "PixelBot"
+
 // Set static folder
 app.use(express.static("public"))
 
 // Run when a client connects
-
 io.on('connection', socket => {
-    console.log("New WebSocket Connection");
+
+    //Welcome The Current User
+    socket.emit('message', formatMessage(botName, 'Welcome To JustChat!'));
+
+    // Broadcast when a user connects
+    socket.broadcast.emit('message', formatMessage(botName,'A user has joined the chat'))
+
+    // Runs when a client disconnects
+    socket.on('disconnect', () => {
+        // Broadcast when a user disconnects
+        socket.broadcast.emit('message', formatMessage(botName,'A user has left the chat'))
+    })
+
+    //Listen for chatMessage from frontend
+    socket.on('chatMessage', msg => {
+        io.emit('message', formatMessage('USER', msg));
+    });
+
 });
 
 const PORT = 3000 || process.env.PORT;
